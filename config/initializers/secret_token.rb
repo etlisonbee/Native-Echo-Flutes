901d92c6f9a0c7bfff3c36247e1cafbf37c629b3
Errol Lisonbee <etlisonbee@gmail.com>
2012-04-23 15:16:49 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NativeEchoFlutes::Application.config.secret_token = 'f59da7c5279237e980c043f456f22a789ad8e26635dcd335c2e2002b468fea26270421c51684bb70bfb03aaffd2d0224db1ad70901a626d76d8e5b46a255308a'
